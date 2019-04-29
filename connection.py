00c7f10c52ecca3a4bd9ca00cb57ec5b833edbe7
Ashwin Shenoy <ashwinshenoy05@gmail.com>
2019-04-29 22:37:19 +0530

    Migrating to firestore

--- connection.py
+++ connection.py
@@ -1,16 +1,41 @@
-import json
+import time 
 import firebase_admin
-from firebase_admin import credentials, db
+import paho.mqtt.client as mqtt 
+from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("smart_city_database_secret.json")
+cred = credentials.Certificate("./smart_city_database_secret.json")
+firebase_admin.initialize_app(cred)
 
-firebase_admin.initialize_app(cred, options={
-    "databaseURL": "https://smart-city-61e3d.firebaseio.com"
-})
+db = firestore.client()
 
-ref = db.reference("house")
-data = ref.set({
-    "button_5": 0
-})
+bots_called = ['garbage']
+def on_connect(client, userdata, flags, rc): 
+   print("Connected with result code " + str(rc)) 
+   client.subscribe("Building/Apartment")	
+	
 
-print(ref.get())
+def on_message(client, userdata, msg):
+	print(bots_called)
+	data = str(msg.payload.decode('utf-8'))
+	bots_called.append(data)
+	print(bots_called[len(bots_called) - 1])
+	# if (bots_called[len(bots_called) - 1] == data):
+	doc_ref = db.collection(u'apartment').document(u'{}'.format(data))
+	doc_ref.set({
+		u'first': u'Ada'
+	})
+	# else:
+	# 	print("Nothing")
+	print(data) 
+	print('-----------------')
+
+client = mqtt.Client() 
+client.on_connect = on_connect 
+client.on_message = on_message 
+client.connect('test.mosquitto.org', 1883, 60) 
+
+client.loop_start()
+
+while True:
+	client.subscribe("Building/Apartment")
+	time.sleep(1)
